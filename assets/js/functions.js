33082c2161c33a3e5bb7d21786866ec86789355e
coucou_miya <bingbing.yan@efrei.net>
2017-01-06 16:36:12 +0100

    restyle some detail

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -9,8 +9,7 @@ function isElementInViewport(el) {
 }
 
 var timelineSections = document.querySelectorAll(".timeline-section");
-function callbackFunc() {
-
+function showTimeLineSection() {
   for (var i = 0; i < timelineSections.length; i++) {
     if (isElementInViewport(timelineSections[i])) {
       timelineSections[i].classList.add("is-showing");
@@ -18,28 +17,56 @@ function callbackFunc() {
   }
 }
 
+function setNavbarPosition(wScroll){
+    if(wScroll > $('header').height())
+        $('.navbar').addClass('fixed');
+    else
+        $('.navbar').removeClass('fixed');
+}
+
+function toggleActive(wScroll){
+    wScroll+= $('.navbar').height();
+    $('.nav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= wScroll &&
+            refElement.position().top + refElement.outerHeight() > wScroll)
+        {
+            //console.log(refElement.height());
+            // var scale = currLink.width()/$(".sliding-indicator").width();
+            // $(".sliding-indicator").css("transform", "scaleX("+scale+")");
+            $('.nav ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
+
+
 // window.addEventListener("load", callbackFunc);
 // window.addEventListener("scroll", callbackFunc);
 $(document).ready(function() {
-    // var wScroll= $(this).scrollTop();
-    // if(wScroll > 50)
-    //   $('.navbar').addClass('white');
     setTimeout(function(){
         $('body').addClass('loaded');
     }, 2800);
-    $("nav a").on('click',function(){
-      var href= $(this).attr('href');
-      var index = href.indexOf("#");
-      href = href.substring(index);
-      console.log(href);
-      $('html, body').animate({
-            scrollTop: $(href).offset().top
+
+    $(".nav a").on('click',function(e){
+        e.preventDefault();
+        var href= $(this).attr('href');
+        var index = href.indexOf("#");
+        href = href.substring(index);
+        $('html, body').animate({
+            scrollTop: $(href).offset().top - $('.navbar').height()
         }, 1000);
     });
+
     $(window).scroll(function(){
-      callbackFunc();
-      var wScroll= $(this).scrollTop();
-      if(wScroll > 50) $('.navbar').addClass('white');
-      else $('.navbar').removeClass('white');
+        showTimeLineSection();
+        var wScroll= $(this).scrollTop();
+        //console.log(wScroll);
+        setNavbarPosition(wScroll);
+        toggleActive(wScroll);
     });
 });
